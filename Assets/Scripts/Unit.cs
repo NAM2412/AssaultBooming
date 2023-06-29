c3d482f091ec01fa1f0d8a28a59c8efa6532828f
NAM2412 <20520643@gm.uit.edu.vn>
2023-06-29 15:03:08 +0700

    Making unit move

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector3 targetPosition;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float stoppingDistance = 0.1f;
+        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
+        {
+            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            float moveSpeed = 4f;
+            transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Move(MouseWorld.GetPosition());
+        }
+    }
+    private void Move(Vector3 targetPosition)
+    {
+        this.targetPosition = targetPosition;
     }
 }
