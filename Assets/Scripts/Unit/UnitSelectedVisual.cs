d5b08b5b2e845301549a8aad62d9e36b8de4d362
NAM2412 <20520643@gm.uit.edu.vn>
2023-07-15 12:11:16 +0700

    Visualizing Selected Unit

---
+++ Assets/Scripts/Unit/UnitSelectedVisual.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AssaultBooming.Unit
+{
+    public class UnitSelectedVisual : MonoBehaviour
+    {
+        [SerializeField] private Unit unit;
+        private MeshRenderer meshRenderer;
+
+        private void Awake()
+        {
+            meshRenderer = GetComponent<MeshRenderer>();
+
+        }
+        private void Start()
+        {
+            UnitActionSystem.Instance.OnSelectedUnitChange += UnitActionSystem_OnSelectedUnitChange;
+            UpdateVisual();
+        }
+        private void UnitActionSystem_OnSelectedUnitChange(object sender, EventArgs empty)
+        {
+            UpdateVisual();
+        }
+
+        private void UpdateVisual()
+        {
+            if (UnitActionSystem.Instance.GetSelectedUnit() == unit)
+            {
+                meshRenderer.enabled = true;
+            }
+            else
+                meshRenderer.enabled = false;
+        }
+    }
+}
+
